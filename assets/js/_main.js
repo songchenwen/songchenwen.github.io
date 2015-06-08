1eaeee1a80d63b835d3ebafa3cea2ff18eba1744
songchenwen <sanvinsung@gmail.com>
2015-06-08 16:14:43 +0800

    Remove Popup Removal Delay

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -28,9 +28,6 @@ $(document).ready(function() {
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
-    removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
-    // make it unique to apply your CSS animations just to this exact popup
     mainClass: 'mfp-fade'
   });
 });
